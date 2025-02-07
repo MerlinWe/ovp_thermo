a3e1b05e3740b107a6e9f068039dea5cbb3fb44a
MerlinWe <merlin.s.weiss@gmail.com>
2025-02-07 15:11:52 +0100

    SEM struggles

--- Analysis/SEM (MWE)/summer_day.R
+++ Analysis/SEM (MWE)/summer_day.R
@@ -19,6 +19,8 @@ library(stats)
 library(lme4)
 library(lmerTest)
 library(performance)
+library(nlme)
+library(piecewiseSEM)
 library(tidyverse)
 
 # Set WD and get source code functions 
@@ -111,15 +113,219 @@ fit_ACT <- assess_model_fit(best_fixed_model_ACT, "mean_activity_percent")
 
 # check top models 
 top_HR <- best_fixed_model_HR
-formula(top_HR)
+top_HR$call
 
 top_BT <- best_fixed_model_BT
-formula(top_BT)
+top_BT$call
 
 top_ACT <- best_fixed_model_ACT
-formula(top_ACT)
+top_ACT$call
 
 
 
-# do SEM modelling for summer day
+
+
+# !!!!!! Experimental from here on ... 
+
+
+
+
+##### ----- SEM modelling for summer day ----- #####
+
+# note: piecewiseSEM does not natively support quadratic terms. 
+# We therefore linearise them before inclusion.
+
+# Create quadratic terms explicitly in data
+summer <- summer %>%
+	mutate(
+		phase_mean_CT_sq = phase_mean_CT^2,
+		day_season_sq = day_season^2,
+		weight_sq = weight^2)
+
+# Refit top models using the transformed quadratic terms
+
+top_HR <- lme(
+	fixed = mean_heartrate ~ season_year + phase_mean_CT + phase_mean_CT_sq + 
+		day_season + day_season_sq + weight + mean_activity_percent,
+	data = summer,
+	random = ~1 + mean_activity_percent | ID_phase, 
+	correlation = corGaus(form = ~ as.numeric(day_season) | ID_phase, nugget = TRUE), 
+	method = "ML", 
+	na.action = na.exclude,
+	control = lmeControl(opt = "optim"))
+
+top_BT <- lme(
+	fixed = mean_BT_smooth ~ season_year + phase_mean_CT + 
+		day_season + day_season_sq + mean_activity_percent,
+	data = summer,
+	random = ~1 | ID_phase, 
+	correlation = corGaus(form = ~ as.numeric(day_season) | ID_phase, nugget = TRUE), 
+	method = "ML", 
+	na.action = na.exclude,
+	control = lmeControl(opt = "optim")
+)
+
+
+top_ACT <- lme(
+	fixed = mean_activity_percent ~ season_year + phase_mean_CT + 
+		weight + weight_sq,
+	data = summer,
+	random = ~1 + phase_mean_CT | ID_phase, 
+	correlation = corGaus(form = ~ as.numeric(day_season) | ID_phase, nugget = TRUE), 
+	method = "ML", 
+	na.action = na.exclude,
+	control = lmeControl(opt = "optim")
+)
+
+# Create the piecewise SEM
+psem_model <- psem(
+	top_HR,
+	top_BT,
+	top_ACT)
+
+# **Check model summary**
+summary(psem_model)
+
+# **Plot the SEM**
+plot(psem_model)
+
+
+### SEM fails... trying a bayesian sem?
+
+library(brms)
+library(bayestestR)
+
+# Prepare dataset (center and scale all continuous variables)
+summer <- summer %>%
+	mutate(
+		phase_mean_CT = scale(phase_mean_CT, center = TRUE, scale = TRUE),
+		phase_mean_CT_sq = phase_mean_CT^2,
+		day_season = scale(day_season, center = TRUE, scale = TRUE),
+		day_season_sq = day_season^2,
+		weight = scale(weight, center = TRUE, scale = TRUE),
+		weight_sq = weight^2,
+		mean_activity_percent = scale(mean_activity_percent, center = TRUE, scale = TRUE)
+	)
+
+# Bayesian model for mean_heartrate
+bayes_HR <- brm(
+	mean_heartrate ~ season_year + phase_mean_CT + phase_mean_CT_sq + 
+		day_season + day_season_sq + weight + mean_activity_percent + 
+		(1 + mean_activity_percent | ID_phase),  # Random slope for repeated measures
+	data = summer,
+	family = gaussian(),
+	prior = c(set_prior("normal(0,1)", class = "b")),  # Regularizing prior
+	chains = 4, cores = 4, iter = 4000, warmup = 1000, 
+	control = list(adapt_delta = 0.95)
+)
+
+bayes_BT <- brm(
+	mean_BT_smooth ~ season_year + phase_mean_CT + 
+		day_season + day_season_sq + mean_activity_percent + 
+		(1 | ID_phase),  # Random intercept
+	data = summer,
+	family = gaussian(),
+	prior = c(set_prior("normal(0,1)", class = "b")),
+	chains = 4, cores = 4, iter = 4000, warmup = 1000, 
+	control = list(adapt_delta = 0.95)
+)
+
+bayes_ACT <- brm(
+	mean_activity_percent ~ season_year + phase_mean_CT + 
+		weight + weight_sq + 
+		(1 + phase_mean_CT | ID_phase),  # Random slope for repeated measures
+	data = summer,
+	family = gaussian(),
+	prior = c(set_prior("normal(0,1)", class = "b")),
+	chains = 4, cores = 4, iter = 4000, warmup = 1000, 
+	control = list(adapt_delta = 0.95)
+)
+
+bayes_SEM <- bf(mean_heartrate ~ season_year + phase_mean_CT + phase_mean_CT_sq + 
+									day_season + day_season_sq + weight + mean_activity_percent + 
+									(1 + mean_activity_percent | ID_phase)) +
+	bf(mean_BT_smooth ~ season_year + phase_mean_CT + 
+		 	day_season + day_season_sq + mean_activity_percent + 
+		 	(1 | ID_phase)) +
+	bf(mean_activity_percent ~ season_year + phase_mean_CT + 
+		 	weight + weight_sq + 
+		 	(1 + phase_mean_CT | ID_phase))
+
+# Fit the full Bayesian SEM
+bayes_model <- brm(
+	formula = bayes_SEM,
+	data = summer,
+	family = gaussian(),
+	prior = c(set_prior("normal(0, 1)", class = "b")),
+	chains = 4, cores = 4, iter = 8000, warmup = 2000,  # Increase iterations
+	control = list(adapt_delta = 0.99, max_treedepth = 15)  # More adaptive sampling
+)
+
+summary(bayes_model)
+pp_check(bayes_model)
+bayestestR::describe_posterior(bayes_model)
+
+# Extract posterior means & 95% credible intervals
+coef_df <- as.data.frame(fixef(bayes_model))
+coef_df <- coef_df %>%
+	mutate(Variable = rownames(coef_df)) %>%
+	select(Variable, Estimate = Estimate)
+
+# Store values in a named list for substitution
+coef_values <- list(
+	HR_BT = round(coef_df$Estimate[coef_df$Variable == "meanheartrate_mean_BT_smooth"], 2),
+	HR_ACT = round(coef_df$Estimate[coef_df$Variable == "meanheartrate_mean_activity_percent"], 2),
+	BT_ACT = round(coef_df$Estimate[coef_df$Variable == "meanBTsmooth_mean_activity_percent"], 2),
+	PHASE_HR = round(coef_df$Estimate[coef_df$Variable == "meanheartrate_phase_mean_CT"], 2),
+	PHASE_BT = round(coef_df$Estimate[coef_df$Variable == "meanBTsmooth_phase_mean_CT"], 2),
+	PHASE_ACT = round(coef_df$Estimate[coef_df$Variable == "meanactivitypercent_phase_mean_CT"], 2),
+	WEIGHT_ACT = round(coef_df$Estimate[coef_df$Variable == "meanactivitypercent_weight"], 2),
+	DAY_HR = round(coef_df$Estimate[coef_df$Variable == "meanheartrate_day_season"], 2),
+	DAY_BT = round(coef_df$Estimate[coef_df$Variable == "meanBTsmooth_day_season"], 2)
+)
+
+# Replace missing coefficients with default values (optional)
+coef_values <- lapply(coef_values, function(x) ifelse(is.na(x), "NA", x))
+
+library(DiagrammeR)
+library(rsvg)
+
+# Save as SVG
+grViz_output <- grViz(sprintf("
+digraph BayesianSEM {
+  node [shape = box, style = filled, fillcolor = lightblue]
+
+  Mean_Heartrate -> Mean_BT_Smooth [label = '%s']
+  Mean_Heartrate -> Mean_Activity_Percent [label = '%s']
+  Mean_BT_Smooth -> Mean_Activity_Percent [label = '%s']
+  Phase_Mean_CT -> Mean_Heartrate [label = '%s']
+  Phase_Mean_CT -> Mean_BT_Smooth [label = '%s']
+  Phase_Mean_CT -> Mean_Activity_Percent [label = '%s']
+  Weight -> Mean_Activity_Percent [label = '%s']
+  Day_Season -> Mean_Heartrate [label = '%s']
+  Day_Season -> Mean_BT_Smooth [label = '%s']
+}
+",
+coef_values$HR_BT,
+coef_values$HR_ACT,
+coef_values$BT_ACT,
+coef_values$PHASE_HR,
+coef_values$PHASE_BT,
+coef_values$PHASE_ACT,
+coef_values$WEIGHT_ACT,
+coef_values$DAY_HR,
+coef_values$DAY_BT
+))
+
+# Convert to SVG and save
+grViz_output %>%
+	export_svg() %>%
+	charToRaw() %>%
+	rsvg_pdf("BayesianSEM.pdf")
+
+# Open the file manually
+browseURL("BayesianSEM.pdf")
+
+
+
 
